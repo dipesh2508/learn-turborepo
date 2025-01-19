75447081ffd515c2e6be9883aced33bf98638564
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2025-01-19 18:35:49 +0530

    feat:added a common module for stuff, setup esbuild

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from "express";
+import {message} from "@repo/common/src"
+
+console.log(message)
 
 let app: Express = express();
 let port:number = 8000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World from Express and TypeScript!");
+  res.send(message);
 });
 
 app.listen(port, () => {
